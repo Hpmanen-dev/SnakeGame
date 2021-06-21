b91ce88fac505a1c15e6ccebd477692f21aa8f6f
Hampus Nilsson <79310444+Hpmanen-dev@users.noreply.github.com>
2021-06-21 17:47:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from snake import Snake
 from apple import Apple
 
 screen = turtle.Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=620, height=620)
 screen.bgcolor("white")
 screen.title('Snake Game')
 
@@ -26,9 +26,17 @@ screen.onkey(Body.right, "Right")
 active = True
 while active:
     screen.update()
+    if Body.head.xcor() > 300 or Body.head.xcor() < -300:
+        print("Game Over!")
+        break
+    if Body.head.ycor() > 300 or Body.head.ycor() < -300:
+        print("Game Over!")
+        break
     Body.move()
     if Body.head.distance(apple.apple) < 19:
         apple.collide()
         Body.add_newbody()
+    
+
 
 turtle.done()
