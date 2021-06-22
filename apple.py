7c23b2dba9ed6fbe9af2ebb0ec77944d460aff65
Hpmanen-dev <79310444+Hpmanen-dev@users.noreply.github.com>
2021-06-22 20:06:08 +0200

    Fixed lag issues.

--- apple.py
+++ apple.py
@@ -10,13 +10,8 @@ class Apple:
         self.apple.goto(20, 20)
     
     def collide(self):
-        new_x = random.randint(-300, 300)
-        while new_x % 20 != 0:
-            new_x = random.randint(-300, 300)
-
-        new_y = random.randint(-300, 300)
-        while new_y % 20 != 0:
-            new_y = random.randint(-300, 300)
+        new_x = random.randrange(-300, 301, 20)
+        new_y = random.randrange(-300, 301, 20)
         
         self.apple.goto(new_x, new_y)
         
